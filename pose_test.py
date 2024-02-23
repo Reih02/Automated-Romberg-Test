17dfc2f295aa187814a40bbd87fba84d932f2d5c
Reilly Haskins <rha192@uclive.ac.nz>
2024-02-23 16:25:50 +1300

    got basic centre of mass display working, yet to implement anatomically-accurate weighted joints

--- pose_test.py
+++ pose_test.py
@@ -3,10 +3,12 @@ from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import matplotlib.image as mpimg
 from matplotlib.pyplot import imshow
-
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import cv2
 
-img = cv2.imread("test.jpg")
+img = cv2.imread("man_falling.jpg")
 
 body_parts = [
     'nose',
@@ -47,7 +49,8 @@ body_parts = [
 
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
+def draw_landmarks_on_image(rgb_image, detection_result, computed_cog):
+  print(f"COMPUTED COG IS : {computed_cog}")
   pose_landmarks_list = detection_result.pose_landmarks
   annotated_image = np.copy(rgb_image)
   # Loop through the detected poses to visualize.
@@ -64,13 +67,58 @@ def draw_landmarks_on_image(rgb_image, detection_result):
       pose_landmarks_proto,
       solutions.pose.POSE_CONNECTIONS,
       solutions.drawing_styles.get_default_pose_landmarks_style())
+    
+  # draw COG
+  height, width, _ = annotated_image.shape
+  x_pixel_location = int(computed_cog[0] * width)
+  y_pixel_location = int(computed_cog[1] * height)
+  print(x_pixel_location)
+  cv2.circle(annotated_image, (x_pixel_location, y_pixel_location), 10, (255, 0, 0), 2)
   return annotated_image
 
+# computes the weighted centre of gravity of subject + plots this on the image
+def compute_cog():
+  
+  x_centre = 0
+  y_centre = 0  
 
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+  weights = {'head': 0.0681/0.5336, 'UPT': 0.1571/0.5336, 'upper_arm': 0.0263/0.5336,
+                     'hand': 0.0059/0.5336, 'LPT': 0.1182/0.5336, 'thigh': 0.1447/0.5336, 'foot': 0.0133/0.5336}
+  bodyparts_classifications = {'nose': 'head', 'left shoulder': 'UPT', 'right shoulder': 'UPT',
+                               'left elbow': 'upper_arm', 'right elbow': 'upper_arm',
+                               'left wrist': 'hand', 'right wrist': 'hand', 'left hip': 'LPT',
+                               'right hip': 'LPT', 'left knee': 'thigh', 'right knee': 'thigh',
+                               'left ankle': 'foot', 'right ankle': 'foot'}
+  
+  with open('body_part_locations.txt', 'r') as file:
+    lines = file.readlines()[1:]
+    joint_locations = []
+    for line in lines:
+        parts = line.split(':')
+        body_part = parts[0].strip()
+        location_str = parts[1].strip()
+        coordinates = location_str[1:-1].split(',')
+        coordinates = tuple(float(coord.strip()) for coord in coordinates)
+        joint_locations.append((body_part, coordinates))
+    counter = 0
+    for i in range(0, len(joint_locations)):
+       if joint_locations[i][0] in bodyparts_classifications:
+         current_bodypart = bodyparts_classifications[joint_locations[i][0]]
+         #print(joint_locations[i][0], joint_locations[i][1])
+         # multiply each joint location with the appropriate segmentation-weight
+         #x_centre += (weights[current_bodypart] * joint_locations[i][1][0])
+         #y_centre += (weights[current_bodypart] * joint_locations[i][1][1])
+         counter += 1
+         x_centre += joint_locations[i][1][0]
+         y_centre += joint_locations[i][1][1]
+    
+    print(counter)
+    x_centre /= 13
+    y_centre /= 13
+    return (x_centre, y_centre)
+    
+
+
 
 # STEP 2: Create an PoseLandmarker object.
 base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
@@ -80,14 +128,12 @@ options = vision.PoseLandmarkerOptions(
 detector = vision.PoseLandmarker.create_from_options(options)
 
 # STEP 3: Load the input image.
-image = mp.Image.create_from_file("test.jpg")
+image = mp.Image.create_from_file("man_falling.jpg")
 
 # STEP 4: Detect pose landmarks from the input image.
 detection_result = detector.detect(image)
 
 pose_landmarks_list = detection_result.pose_landmarks
-
-landmark_locations = {}
 
 with open('body_part_locations.txt', 'w') as file:
   file.write("Locations (xyz) grouped by body part\n")
@@ -97,11 +143,12 @@ with open('body_part_locations.txt', 'w') as file:
     current_location = (current_landmark.x, current_landmark.y, current_landmark.z)
     file.write(f"{current_body_part}: {current_location}\n")
 
-print(pose_landmarks_list[0][12])
-print(pose_landmarks_list[0][11])
+
+
+computed_cog = compute_cog()
 
 # STEP 5: Process the detection result. In this case, visualize it.
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result, computed_cog)
 while cv2.waitKey(1) < 0:
   cv2.imshow('', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 #cv2.waitKey(1)
