012659847a9ed01cd2506fd8cd15fe0268a553f9
Reilly Haskins <rha192@uclive.ac.nz>
2024-02-27 16:27:56 +1300

    implemented functionality to calculate weight distribution difference across feet + COG smoother function

--- pose_video_test.py
+++ pose_video_test.py
@@ -8,9 +8,11 @@ from matplotlib.pyplot import imshow
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from filterpy.kalman import KalmanFilter
+import math
 
 
-video_path = 'captured_video/sample_vid.mp4'
+video_path = 'captured_video/positive_diagnosis_clipped.mp4'
 vid = cv2.VideoCapture(video_path)
 
 body_parts = [
@@ -50,11 +52,11 @@ body_parts = [
 ]
 
 
-def draw_landmarks_on_image(rgb_image, detection_resul, computed_cog):
-  print(f"COMPUTED COG IS : {computed_cog}")
+def draw_landmarks_on_image(rgb_image, detection_result, computed_cog, smoothed_cog, unbalanced, right_foot, left_foot):
+  #print(f"COMPUTED COG IS : {computed_cog}")
   pose_landmarks_list = detection_result.pose_landmarks
   annotated_image = np.copy(rgb_image)
-  print(pose_landmarks_list[0])
+  #print(pose_landmarks_list[0])
   # Loop through the detected poses to visualize.
   for idx in range(len(pose_landmarks_list)):
     pose_landmarks = pose_landmarks_list[idx]
@@ -74,6 +76,23 @@ def draw_landmarks_on_image(rgb_image, detection_resul, computed_cog):
   x_pixel_location = int(computed_cog[0] * width)
   y_pixel_location = int(computed_cog[1] * height)
   cv2.circle(annotated_image, (x_pixel_location, y_pixel_location), 10, (255, 0, 0), 2)
+
+  # draw lines between foot and COG
+  right_foot_x, right_foot_y = int(right_foot[0] * width), int(right_foot[1] * height)
+  left_foot_x, left_foot_y = int(left_foot[0] * width), int(left_foot[1] * height)
+  cv2.line(annotated_image, (right_foot_x, right_foot_y), (x_pixel_location, y_pixel_location), (120, 0, 120), 2)
+  cv2.line(annotated_image, (left_foot_x, left_foot_y), (x_pixel_location, y_pixel_location), (120, 0, 120), 2)
+
+  # draw smoothed COG
+  smooth_x_pixel_location = int(smoothed_cog[0] * width)
+  smooth_y_pixel_location = int(smoothed_cog[1] * height)
+  cv2.circle(annotated_image, (smooth_x_pixel_location, smooth_y_pixel_location), 15, (0, 0, 255), 2)
+
+  if unbalanced:
+    cv2.circle(annotated_image, (50, 50), 5, (0, 0, 255), 5)
+
+  
+
   return annotated_image
 
 # computes the weighted centre of gravity of subject + plots this on the image
@@ -114,6 +133,56 @@ def compute_cog():
     x_centre /= used_joints
     y_centre /= used_joints
     return (x_centre, y_centre)
+  
+class SmoothCOG:
+  def __init__(self, alpha):
+    self.alpha = alpha
+    self.smoothed_x = 0
+    self.smoothed_y = 0
+    self.used = False
+  
+  def smooth_data(self, current_cog):
+    if not self.used:
+      self.used = True
+      self.smoothed_x = current_cog[0]
+      self.smoothed_y = current_cog[1]
+    else:
+      self.smoothed_x = self.alpha * current_cog[0] + (1 - self.alpha) * self.smoothed_x
+      self.smoothed_y = self.alpha * current_cog[1] + (1 - self.alpha) * self.smoothed_y
+
+    return (self.smoothed_x, self.smoothed_y)
+  
+# Calculates how much relative weight is stored on each foot based on pose geometry and
+# calculated centre of mass
+def calculate_weight_distribution(cog):
+  with open('body_part_locations.txt', 'r') as file:
+    lines = file.readlines()[-2:]
+    joint_locations = []
+    for line in lines:
+        parts = line.split(':')
+        body_part = parts[0].strip()
+        location_str = parts[1].strip()
+        coordinates = location_str[1:-1].split(',')
+        coordinates = tuple(float(coord.strip()) for coord in coordinates)
+        joint_locations.append(coordinates[0:2])
+
+    right_foot, left_foot = joint_locations[0], joint_locations[1]
+    distance_right_foot = math.sqrt((right_foot[0] - cog[0])**2 + (right_foot[1] - cog[1])**2)
+    distance_left_foot = math.sqrt((left_foot[0] - cog[0])**2 + (left_foot[1] - cog[1])**2)
+
+    weight_distro_right = (1 / distance_right_foot) * (1 / (distance_right_foot + distance_left_foot)) * 100
+    weight_distro_left = (1 / distance_left_foot) * (1 / (distance_right_foot + distance_left_foot)) * 100
+
+    # normalise vals
+    #right_ratio = distance_right_foot / (distance_left_foot + distance_right_foot)
+    #left_ratio = distance_left_foot / (distance_left_foot + distance_right_foot)
+
+    # print(f"WEIGHT DISTRO RIGHT: {right_ratio}")
+    # print(f"WEIGHT DISTRO LEFT: {left_ratio}")
+
+    return (weight_distro_right, weight_distro_left, right_foot, left_foot)
+
+  
 
 # Create a PoseLandmarker object.
 base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
@@ -121,6 +190,14 @@ options = vision.PoseLandmarkerOptions(
     base_options=base_options,
     output_segmentation_masks=True)
 detector = vision.PoseLandmarker.create_from_options(options)
+  
+# Init COG smoother class with defined alpha val
+alpha = 0.5
+data_smoother = SmoothCOG(alpha)
+
+patient_was_unbalanced = False
+
+max_ratio_difference = 0
 
 # Load the input frames from the video.
 while cv2.waitKey(1) < 0:
@@ -145,9 +222,32 @@ while cv2.waitKey(1) < 0:
 
   computed_cog = compute_cog()
 
-  annotated_image = draw_landmarks_on_image(rgb_frame.numpy_view(), detection_result, computed_cog)
+  smoothed_cog = data_smoother.smooth_data(computed_cog)
+  #print(f"RAW COG: {computed_cog[0], computed_cog[1]}")
+  #print(f"SMOOTH COG: {smoothed_cog[0], smoothed_cog[1]}")
+
+  right_ratio, left_ratio, right_foot, left_foot = calculate_weight_distribution(computed_cog)
+
+  print(f"Weight distribution difference is: {abs(right_ratio - left_ratio)}%")
+
+  if abs(right_ratio - left_ratio) > max_ratio_difference:
+    max_ratio_difference = abs(right_ratio - left_ratio)
+
+  # check if subject is "unbalanced"
+  if abs(right_ratio - left_ratio) > 6:
+    print("###UNBALANCED###")
+    patient_was_unbalanced = True
+    unbalanced = True
+  else:
+    unbalanced = False
+
+  annotated_image = draw_landmarks_on_image(rgb_frame.numpy_view(), detection_result, computed_cog, smoothed_cog, unbalanced, right_foot, left_foot)
   cv2.imshow('Frame', annotated_image)
 
 vid.release()
 cv2.destroyAllWindows()
+
+print(f"Max ratio difference: {max_ratio_difference}")
+
+print(f"Diagnosis: {patient_was_unbalanced}")
 
